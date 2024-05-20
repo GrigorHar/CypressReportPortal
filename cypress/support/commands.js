3ba168691e8925f8416e7851f6796d486fd8ed8c
Grigor Harutyunyan <grigor_harutyunyan@epam.com>
2024-05-20 15:06:52 +0400

    Module 8 homework

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,42 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('waitForElementToBeVisible', (selector, timeout = 10000) => {
+    cy.get(selector, { timeout }).should('be.visible');
+});
+
+Cypress.Commands.add('waitForElementToBeClickable', (selector, timeout = 10000) => {
+    cy.get(selector, { timeout }).should('not.be.disabled').and('be.visible');
+});
+
+Cypress.Commands.add('dragAndDrop', (sourceSelector, targetSelector) => {
+    cy.get(sourceSelector).trigger('mousedown', { which: 1 });
+    cy.get(targetSelector)
+        .trigger('mousemove', { force: true })
+        .trigger('mouseup', { force: true });
+});
+
+Cypress.Commands.add('resizeElement', (selector, widthChange, heightChange) => {
+    cy.get(selector)
+        .trigger('mousedown', { which: 1 })
+        .trigger('mousemove', { clientX: widthChange, clientY: heightChange })
+        .trigger('mouseup');
+});
+
+Cypress.Commands.add('scrollToElement', (selector) => {
+    cy.get(selector).scrollIntoView();
+});
+
+Cypress.Commands.add('isScrolledIntoView', (selector) => {
+    cy.get(selector).then($el => {
+        const bottom = Cypress.$(cy.window().height());
+        const rect = $el[0].getBoundingClientRect();
+        expect(rect.top).to.be.lessThan(bottom);
+        expect(rect.bottom).to.be.greaterThan(0);
+    });
+});
+
+Cypress.Commands.add('hover', { prevSubject: 'element' }, (subject) => {
+    cy.wrap(subject).trigger('mouseover');
+});
--- cypress/support/po/pages/BasePage.js
+++ cypress/support/po/pages/BasePage.js
@@ -2,4 +2,16 @@ export default class BasePage{
     getElementThatContains(text) {
         return cy.contains(text);
     }
+
+    getElement(selector) {
+        return cy.get(selector);
+    }
+
+    clickElement(selector) {
+        this.getElement(selector).click();
+    }
+
+    typeInElement(selector, text) {
+        this.getElement(selector).clear().type(text);
+    }
 }
